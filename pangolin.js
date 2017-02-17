902348255eba86ae38b3738ad234f8f7837723c3
Julia Janicki <jhjanicki@gmail.com>
2017-02-17 17:46:59 +0900

    first commit

---
+++ pangolin.js
@@ -0,0 +1,345 @@
+var pangolins = 
+[{
+    "id": "Indian Pangolin", 
+    "idd":"IndianPangolin",
+    "description": "The Indian pangolin is the largest of all the Asian pangolin species, and can grow to be 1.2m long (nose to tail).This species is largely terrestrial, and forages primarily on the ground for ants, termites and occasionally other invertebrates. Interestingly, where this species occurs in Sri Lanka it inhabits tropical rainforest, where it is believed to lead a more arboreal lifestyle - using its claws and prehensile tail to aid climbing. This species is native to India, Nepal, Pakistan and Sri Lanka.",
+    "status": "Endangered",
+    "image":"https://dl.dropboxusercontent.com/u/102373131/pangolins/indianPangolin.jpg",
+    "data": [
+        {
+            "threat": "Local Hunting / Domestic Trade", 
+            "note":"Meat + Scales",
+            "count": 7
+        }, 
+        {
+            "threat": "International Trade", 
+            "count": 9
+        }, 
+        {
+            "threat": "Habitat Loss", 
+            "count": 6
+        }, 
+        {
+            "threat": "Pesticide use", 
+            "count": 5
+        }, 
+        {
+            "threat": "Deforestation", 
+            "count": 0
+        }, 
+        {
+            "threat": "Road Kill", 
+            "count": 0
+        },
+        {
+            "threat": "Habitat degradation", 
+            "count": 0
+        },
+        {
+            "threat": "Electrocution", 
+            "count": 0
+        }
+    ]
+},
+{
+    "id": "Philippine Pangolin", 
+    "idd":"PhilippinePangolin",
+    "description": "This species is endemic to the Philippines, where it is found in the Palawan faunal region. This species is elusive, it is solitary and nocturnal and not so much is known about it. They are found in forests, grasslands and scrublands. As with other pangolins, this species feeds on termites and ants. ",
+    "status": "Endangered",
+    "image":"https://dl.dropboxusercontent.com/u/102373131/pangolins/philippinePangolin.jpg",
+    "data": [
+        {
+            "threat": "Local Hunting / Domestic Trade", 
+            "note": "Meat + Scales + Skin",
+            "count": 7
+        }, 
+        {
+            "threat": "International Trade", 
+            "note": "Meat + Scales + Skin + Whole Animal",
+            "count": 7
+        }, 
+        {
+            "threat": "Habitat Loss", 
+            "count": 5
+        }, 
+        {
+            "threat": "Pesticide use", 
+            "count": 0
+        }, 
+        {
+            "threat": "Deforestation", 
+            "count": 4
+        }, 
+        {
+            "threat": "Road Kill", 
+            "count": 0
+        },
+        {
+            "threat": "Habitat degradation", 
+            "count": 0
+        },
+        {
+            "threat": "Electrocution", 
+            "count": 0
+        }
+    ]
+},
+{
+    "id": "Sunda Pangolin", 
+    "idd":"SundaPangolin",
+    "description": "The Sunda Pangolin which has the largest range of any Asian species, being found across mainland and island Southeast Asia. It is a species heavily threatened by trade as it is hunted locally as a protein source and for the international trade in skins, scales and meat, mostly for traditional Chinese medicine based on false beliefs. As with other pangolins, this species is primarily nocturnal, solitary and a specialized feeder on ants and termites. ",
+    "status": "Critically Endangered",
+    "image":"https://dl.dropboxusercontent.com/u/102373131/pangolins/SundaPangolin.jpg",
+    "data": [
+        {
+            "threat": "Local Hunting / Domestic Trade", 
+            "count": 3
+        }, 
+        {
+            "threat": "International Trade", 
+            "note": "Meat + Scales + Skin + Whole Animal",
+            "count": 10
+        }, 
+        {
+            "threat": "Habitat Loss", 
+            "count": 7
+        }, 
+        {
+            "threat": "Pesticide use", 
+            "count": 0
+        }, 
+        {
+            "threat": "Deforestation", 
+            "count": 4
+        }, 
+        {
+            "threat": "Road Kill", 
+            "count": 1
+        },
+        {
+            "threat": "Habitat degradation", 
+            "count": 0
+        },
+        {
+            "threat": "Electrocution", 
+            "count": 0
+        }
+    ]
+},
+{
+    "id": "Chinese Pangolin", 
+    "idd":"ChinesePangolin",
+    "description": "This species is found in a wide range of habitats, from forests to grasslands to agricultural fields. The species digs its own burrows, or enlarges passages made by termites.  The primary threat to this species is hunting and poaching for domestic and especially international trade, which is now driven largely by market demand in China, where pangolin meat is consumed as a luxury product and its scales used in traditional medicines.  In China, where the Chinese pangolin is thought to be commercially extinct, the pangolin is called 鲮 鲤 (Líng lǐ), which translates literally as 'mud carp' - referring to the fish-like scales which cover the body. It is also found in Taiwan, Hong Kong and parts of South East Asia.",
+    "status": "Critically Endangered",
+    "image":"https://dl.dropboxusercontent.com/u/102373131/pangolins/ChinesePangolin.jpg",
+    "data": [
+        {
+            "threat": "Local Hunting / Domestic Trade", 
+            "note": "Meat as a protein source + Scales for use in traditional medicines",
+            "count": 7
+        }, 
+        {
+            "threat": "International Trade", 
+            "note": "Meat + Scales + Skin + Whole Animal",
+            "count": 10
+        }, 
+        {
+            "threat": "Habitat Loss", 
+            "count": 0
+        }, 
+        {
+            "threat": "Pesticide use", 
+            "count": 0
+        }, 
+        {
+            "threat": "Deforestation", 
+            "count": 0
+        }, 
+        {
+            "threat": "Road Kill", 
+            "count": 0
+        },
+        {
+            "threat": "Habitat degradation", 
+            "count": 0
+        },
+        {
+            "threat": "Electrocution", 
+            "count": 0
+        }
+    ]
+},
+{
+    "id": "Black-bellied Pangolin", 
+    "idd":"BlackbelliedPangolin",
+    "description": "The black-bellied pangolin is probably the species about which we have the most still to learn. It is thought to be strictly arboreal, inhabiting swamp and riverine forests of West and Central Africa, where it feeds on the tree ants forced up into the branches to avoid flooding. This species is also known as the long-tailed pangolin -  These pangolins have 47 tail vertebrae - the most of any mammal species! Also equipped with a sensitive pad at the tip, the black-bellied pangolin's tail gives the agility it needs for its arboreal lifestyle.",
+    "status": "Vulnerable",
+    "image":"https://dl.dropboxusercontent.com/u/102373131/pangolins/blackbelliedPangolin.png.jpg",
+    "data": [
+        {
+            "threat": "Local Hunting / Domestic Trade", 
+            "note": "Meat for consumption + Scales for for cultural and ethno-medicinal purposes including traditional African medicine",
+            "count": 8
+        }, 
+        {
+            "threat": "International Trade", 
+            "note": "Meat + Scales + Skin + Whole Animal",
+            "count": 8
+        }, 
+        {
+            "threat": "Habitat Loss", 
+            "count": 6
+        }, 
+        {
+            "threat": "Pesticide use", 
+            "count": 0
+        }, 
+        {
+            "threat": "Deforestation", 
+            "count": 0
+        }, 
+        {
+            "threat": "Road Kill", 
+            "count": 0
+        },
+        {
+            "threat": "Habitat degradation", 
+            "count": 4
+        },
+        {
+            "threat": "Electrocution", 
+            "count": 0
+        }
+    ]
+},
+{
+    "id": "White-bellied Pangolin", 
+    "idd":"WhitebelliedPangolin",
+    "description": "This species is found in is found in West and Central Africa. The white-bellied pangolin is also known as the tree pangolin, although it is thought to be only semi arboreal - happy to forage both on the ground and in the branches.  The scientific name for the white-bellied pangolin is <i>Phataginus tricuspis</i>. <i>Tricuspis</i> - meaning three-cusped - refers to the distinctive shape of the scales, which end in three points. The pointed tips to the scales are often easier to see in younger individuals than in older ones, as they can wear smooth with age.",
+    "status": "Vulnerable",
+    "image":"https://dl.dropboxusercontent.com/u/102373131/pangolins/whitebelliedPangolin.jpg",
+    "data": [
+        {
+            "threat": "Local Hunting / Domestic Trade", 
+            "note": "Bushmeat and traditional African medicine",
+            "count": 10
+        }, 
+        {
+            "threat": "International Trade", 
+            "note": "Meat + Scales + Skin + Whole Animal",
+            "count": 8
+        }, 
+        {
+            "threat": "Habitat Loss", 
+            "count": 6
+        }, 
+        {
+            "threat": "Pesticide use", 
+            "count": 0
+        }, 
+        {
+            "threat": "Deforestation", 
+            "count": 0
+        }, 
+        {
+            "threat": "Road Kill", 
+            "count": 0
+        },
+        {
+            "threat": "Habitat degradation", 
+            "count": 5
+        },
+        {
+            "threat": "electrocution", 
+            "count": 0
+        }
+    ]
+},
+{
+    "id": "Giant Ground Pangolin", 
+    "idd":"GiantGroundPangolin",
+    "description": " Of the four African pangolin species, ground pangolins are the species about which we know the most. For instance, research shows that ground pangolins alter their activity patterns seasonally: although strictly nocturnal during hot summers, ground pangolins are known to forage in daylight hours during cooler months. As its name suggests, this is the largest of the pangolin species, weighing up to 35 kg. They inhabit the dense forests of West and Central Africa, and are always found near water sources.  They have large claws on their front legs, which they use to tear into termite mounds and dig burrows. When walking, they curl these claws backwards and inwards, so the weight falls onto the outside of the wrist.",
+    "status": "Vulnerable",
+    "image":"https://dl.dropboxusercontent.com/u/102373131/pangolins/giantGroundPangolin.jpg",
+    "data": [
+        {
+            "threat": "Local Hunting / Domestic Trade", 
+            "note": "Bushmeat and for cultural and ethno-medicinal purposes including traditional African medicine",
+            "count": 8
+        }, 
+        {
+            "threat": "International Trade", 
+            "note": "Meat + Scales + Skin + Whole Animal",
+            "count": 7
+        }, 
+        {
+            "threat": "Habitat Loss", 
+            "count": 0
+        }, 
+        {
+            "threat": "Pesticide use", 
+            "count": 0
+        }, 
+        {
+            "threat": "Deforestation", 
+            "count": 0
+        }, 
+        {
+            "threat": "Road Kill", 
+            "count": 0
+        },
+        {
+            "threat": "Habitat degradation", 
+            "count": 6
+        },
+        {
+            "threat": "Electrocution", 
+            "count": 0
+        }
+    ]
+},
+{
+    "id": "Temmincks Ground Pangolin", 
+    "idd":"TemmincksGroundPangolin",
+    "description": "This is a predominantly solitary, terrestrial species that inhabits mainly savanna woodland in low-lying regions. The species is eaten as bushmeat across its range (e.g. South Africa, Zimbabwe, Namibia, Mozambique, Tanzania). Of greater threat is overexploitation for body parts and scales which have superstitious value and are used for medicinal purposes. It is also threatened by international trade, as the demand for, and price of, pangolin products in Asia is increasing, while the supply from the Asian species is decreasing.",
+    "status": "Vulnerable",
+    "image":"https://dl.dropboxusercontent.com/u/102373131/pangolins/temnicksPangolin.jpeg",
+    "data": [
+        {
+            "threat": "Local Hunting / Domestic Trade", 
+            "note": "Bushmeat + body parts and scales that have superstitious value and are used for medicinal purposes",
+            "count": 9
+        }, 
+        {
+            "threat": "International Trade", 
+            "note": "Meat + Scales + Skin + Whole Animal",
+            "count": 7
+        }, 
+        {
+            "threat": "Habitat Loss", 
+            "count": 6
+        }, 
+        {
+            "threat": "Pesticide use", 
+            "count": 0
+        }, 
+        {
+            "threat": "Deforestation", 
+            "count": 0
+        }, 
+        {
+            "threat": "Road Kill", 
+            "count": 4
+        },
+        {
+            "threat": "Habitat degradation", 
+            "count": 0
+        },
+        {
+            "threat": "Electrocution", 
+            "count": 5
+        }
+    ]
+}
+
+]
